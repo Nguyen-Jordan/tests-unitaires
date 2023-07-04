518b93c6224a2500f18cb2ebf5ae10441dd2f461
Nguyen-Jordan <dizorder3@gmail.com>
2023-07-04 14:55:03 +0200

    add getLetter, getWord, isInclude, letsTry, isLost, isAlreadyInsert, pushOnTable, showPlayerGuess, isWin functions & tests

--- index.js
+++ index.js
@@ -1,8 +1,71 @@
-
-export function hanging (letter, word) {
-    let convertLetter = letter.toLowerCase()
-    let convertWord = word.toLowerCase()
-    return convertWord.includes(convertLetter)
+// demander le prompt et le traité
+export function getLetter(letter) {
+    return letter.toUpperCase()
 }
 
-// export function letsPlay ()
+// choisir un mot du tableau aléatoirement
+export function getWord(table) {
+    return table[Math.floor(Math.random() * table.length)].toUpperCase();
+}  
+
+// comparé la lettre et le mot
+export function isInclude (letter, word) {
+    return word.includes(letter)
+}
+
+// condition pour la lettre correspond au mot on l'ajoute au tableau sinon on réduit le compteur d'essai
+ export function letsTry (bool){
+    let countDown = 7;
+    if (!bool){
+        return countDown -= 1     
+    }
+    return countDown
+ }
+
+  // condition pour le compteur d'essai arrive a zero c'est perdu
+  export function isLost (count){
+    if (count === 0){
+        return true
+    }
+        return false
+  }
+
+  // condition pour eviter le doublon
+  export function isAlreadyInsert (letter, table) {
+    if (!table.includes(letter)){
+        return false
+    }
+    return true
+  }
+
+ // condition pour le succes on ajoute la lettre au tableau
+ export function pushOnTable (letter, word, table) {
+    if (!word.includes(letter)) {
+        const message = "I'm sorry"
+        return message
+    }
+    table.push(letter)
+    return table
+ }
+
+ // affichage du mot cache
+ export function showPlayerGuess (word, wordsGuess) {
+    let wordHidden = ''
+    for (let letter of word) {
+        if (wordsGuess.includes(letter)) {
+            wordHidden += letter
+        } else {
+            wordHidden += '-'
+        }
+    }
+    return wordHidden
+ }
+
+ // verifier si c'est gagne
+ export function isWin (word, wordHidden) {
+    if(wordHidden === word){
+        return true
+    } else {
+        return false
+    }
+ }
--- test/hang.test.js
+++ test/hang.test.js
@@ -1,14 +1,81 @@
 import { test, expect } from 'vitest';
-import { hanging } from '../index.js';
+import 
+{ 
+    getLetter, getWord, isInclude,
+    letsTry, isLost,isAlreadyInsert,
+    pushOnTable, showPlayerGuess, isWin
+} from '../index.js';
 
-test('contain leter on the word', () => {
-    expect(hanging('j', 'jordan')).toBe(true)
-    expect(hanging('v','jordan')).toBe(false)
-    expect(hanging('6','jordan')).toBe(false)
-    expect(hanging('$','jordan')).toBe(false)
-    expect(hanging('J','jordan')).toBe(true)
-    expect(hanging('m','MAPPLE')).toBe(true)
-    expect(hanging('jo','jo')).toBe(false)
+test('return a valid letter', () => {
+    expect(getLetter('j')).toBe('J')
+    expect(getLetter('J')).toBe('J')
 })
 
-test('')
+test('get a word from the table', () => {
+    const table = ['HELLO', 'CAT', 'FLOWER']
+    const word = getWord(table)
+    expect(table).toContain(word)
+})
+
+test('contain letter on the word', () => {
+    expect(isInclude('V','JORDAN')).toBe(false)
+    expect(isInclude('J','JORDAN')).toBe(true)
+})
+
+test('lets try count down', () => {
+    let countDown = 7;
+    expect(letsTry(false)).toBe(6)
+    expect(letsTry(true)).toBe(7)
+})
+
+test('verify if the player loses the game', () => {
+    let countDown = 1
+    let lastchance = 0
+    expect(isLost(countDown)).toBe(false)
+    expect(isLost(lastchance)).toBe(true)
+
+})
+
+test('letter already choose', () => {
+    let table = ['H','E','L']
+    expect(isAlreadyInsert('H',table)).toBe(true)
+    expect(isAlreadyInsert('E',table)).toBe(true)
+    expect(isAlreadyInsert('L',table)).toBe(true)
+    expect(isAlreadyInsert('G',table)).toBe(false)
+})
+
+test('lets add it on the player table', () => {
+    let table = []
+    const word = 'HELLO'
+    expect(pushOnTable('H', word, table)).toStrictEqual(['H'])
+    expect(pushOnTable('E', word, table)).toStrictEqual(['H','E'])
+    expect(pushOnTable('L', word, table)).toStrictEqual(['H','E','L'])
+    expect(pushOnTable('O', word, table)).toStrictEqual(['H','E','L','O'])
+    expect(pushOnTable('G', word, table)).toBe("I'm sorry")
+})
+
+test('show player words hidden', () => {
+    const ho = ['H','O']
+    const l = ['L']
+    const eo = ['E','O']
+    const el = ['E','L']
+    const lo = ['L','O']
+    const hello = ['H','E','L','O']
+    const empty = []
+    const word = 'HELLO'
+    expect(showPlayerGuess(word,ho)).toBe('H---O')
+    expect(showPlayerGuess(word,l)).toBe('--LL-')
+    expect(showPlayerGuess(word,eo)).toBe('-E--O')
+    expect(showPlayerGuess(word,el)).toBe('-ELL-')
+    expect(showPlayerGuess(word,lo)).toBe('--LLO')
+    expect(showPlayerGuess(word,hello)).toBe('HELLO')
+    expect(showPlayerGuess(word,empty)).toBe('-----')
+    console.log(showPlayerGuess(word,lo))
+})
+
+test('verify if the player win', () => {
+    expect(isWin('HELLO', 'HELLO')).toBe(true)
+    expect(isWin('HELLO', 'H-LL-')).toBe(false)
+    expect(isWin('HELLO', 'HE--O')).toBe(false)
+    expect(isWin('HELLO', '-----')).toBe(false)
+})
